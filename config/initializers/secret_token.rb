247b8a8e414517aa929493a71dcbefa4b74b20e9
Serena Wales <serenawr@gmail.com>
2012-02-22 19:07:12 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuideToAmerica::Application.config.secret_token = '40af463a6035e267f55733a386065ac0e63c71e310ddcf0184c313ffb973ea30237f11dd081a2993715ae901413246f7853f87087a30e0b98a162ca29e13cc0a'
