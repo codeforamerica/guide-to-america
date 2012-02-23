d5b4e6efdecd805f4cec345828096f2c9f05c140
Serena Wales <serenawr@gmail.com>
2012-02-22 20:04:23 -0600

    User is devise-enabled

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :name, :position, :home_city, :year, :profession, :email, :password, :password_confirmation, :remember_me
 end
