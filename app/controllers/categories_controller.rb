31667ef2ce13fc23be22d03032306cf21f8dcdd8
Serena Wales <serenawr@gmail.com>
2012-02-22 23:36:33 -0600

    Output JSON in category controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,9 @@
 class CategoriesController < ApplicationController
   def index
     @categories = Category.all
+    respond_to do |format|
+      format.html
+      format.json { render :json => @categories }
+    end
   end
 end
